53fc91d23de417b4b95da6876df7304966cab642
YNAGULAN S <nagulan1645@gmail.com>
2021-06-18 20:32:29 +0530

    google_analytics

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 import React, { Component } from 'react';
 import { BrowserRouter as Router , Switch , Route,Redirect} from 'react-router-dom';
 import Whatsapp from './components/whatsapp';
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-DVP8025EJK');
 
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 class App extends Component {
   state = {  }
   render() { 
